12a8ccf3ceedb0de762766f24690e4a603f0b23e
NorbiPeti <szatmari.norbert.peter@gmail.com>
2021-02-21 01:39:51 +0100

    Use bStats for metrics and remove Piwik integration
    #4

--- src/main/java/de/jaschastarke/minecraft/limitedcreative/FeatureMetrics.java
+++ src/main/java/de/jaschastarke/minecraft/limitedcreative/FeatureMetrics.java
@@ -1,69 +1,46 @@
 package de.jaschastarke.minecraft.limitedcreative;
 
-import java.io.IOException;
-
-import org.bukkit.event.Listener;
-
 import de.jaschastarke.bukkit.lib.CoreModule;
-import de.jaschastarke.bukkit.tools.stats.IStatistics;
-import de.jaschastarke.bukkit.tools.stats.PiwikStatistics;
 import de.jaschastarke.modularize.IModule;
 import de.jaschastarke.modularize.ModuleEntry;
 import de.jaschastarke.modularize.ModuleEntry.ModuleState;
+import org.bstats.bukkit.Metrics;
+import org.bstats.charts.MultiLineChart;
+import org.bukkit.event.Listener;
+
+import java.util.HashMap;
 
 public class FeatureMetrics extends CoreModule<LimitedCreative> implements Listener {
     public FeatureMetrics(LimitedCreative plugin) {
         super(plugin);
     }
-    private IStatistics metric;
-    private MCStatsMetrics mcstats = null;
-    
+
+    private Metrics bstats = null;
+
     @Override
     public void onEnable() {
         super.onEnable();
-        metric = new PiwikStatistics(plugin);
-        if (mcstats == null) {
-            try {
-                mcstats = new MCStatsMetrics(plugin);
+        if (bstats == null) {
+            bstats = new Metrics(plugin, 10413);
 
-                MCStatsMetrics.Graph moduleGraph = mcstats.createGraph("Module Usage");
-                for (final ModuleEntry<IModule> mod : plugin.getModules()) {
-                    if (mod.getModule() instanceof CoreModule<?>) {
-                        moduleGraph.addPlotter(new MCStatsMetrics.Plotter(((CoreModule<?>) mod.getModule()).getName()) {
-                            @Override
-                            public int getValue() {
-                                return mod.getState() == ModuleState.ENABLED ? 1 : 0;
-                            }
-                        });
-                    }
-                }
-                MCStatsMetrics.Graph depGraph = mcstats.createGraph("Dependencies");
-                for (final String dep : plugin.getDescription().getSoftDepend()) {
-                    depGraph.addPlotter(new MCStatsMetrics.Plotter(dep) {
-                        @Override
-                        public int getValue() {
-                            return plugin.getServer().getPluginManager().isPluginEnabled(dep) ? 1 : 0;
-                        }
-                    });
-                }
-                
-                mcstats.start();
-            } catch (IOException e) {
-                // Failed to submit the stats :-(
-                getLog().warn("MCStats-Error: " + e.getMessage());
-            }
+            bstats.addCustomChart(new MultiLineChart("Module Usage", () -> {
+                HashMap<String, Integer> ret = new HashMap<>();
+                for (final ModuleEntry<IModule> mod : plugin.getModules())
+                    if (mod.getModule() instanceof CoreModule<?>)
+                        ret.put(((CoreModule<?>) mod.getModule()).getName(), mod.getState() == ModuleState.ENABLED ? 1 : 0);
+                return ret;
+            }));
+            bstats.addCustomChart(new MultiLineChart("Dependencies", () -> {
+                HashMap<String, Integer> ret = new HashMap<>();
+                for (final String dep : plugin.getDescription().getSoftDepend())
+                    ret.put(dep, plugin.getServer().getPluginManager().isPluginEnabled(dep) ? 1 : 0);
+                return ret;
+            }));
         }
     }
-    
+
     @Override
     public void onDisable() {
         super.onDisable();
-        metric.unregister();
-    }
-
-    public void track(String event) throws IOException {
-        if (metric == null)
-            throw new IllegalAccessError("The feature hasn't been enabled");
-        metric.trackEvent(event);
     }
 }
